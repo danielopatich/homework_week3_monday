0ab48447069e36c3acbe50a14ce8e7ba8e22c240
Daniel Opatich <daniel.opatich@gmail.com>
2015-10-19 14:40:40 -0500

    homey worky

---
+++ functions.js
@@ -0,0 +1,39 @@
+// NUMBERO UNO //
+function sum(a, b) {
+    c = a + b;
+    return c;}
+console.log((sum( 2, 3 )));
+
+// NUMBERO DOS //
+function avg(a, b, c) {
+    d = a + b + c;
+    e = d/3;
+  	return e; }
+	console.log(avg(2, 3, 4));
+
+// NUMBERO TRES //
+function getLength(greeting){
+    x = greeting.length;
+    return x;}
+	console.log(getLength('Hey Dude'));
+
+// NUMBERO CUATRO //
+function greaterThan(a, b){
+    if (a < b) {
+    return 'True'
+    } else {
+    	return 'False';
+    }}
+    console.log(greaterThan(2 , 3));
+
+// NUMBERO CINCO //
+function greet(name){
+  return 'Hello, ' + name + '!'
+  }
+  console.log(greet('Jim Bob'));
+
+// NUMBERO TRES //
+function madlib(a, b, c, d){
+  return a + b + 'like ' + c + d
+}
+  console.log(madlib('fluffy ','dogs ','warm ','hugs '))
